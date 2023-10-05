8d67802dffefbca8c9b8b74ab1d6300703a88bb2
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-10-05 11:51:45 +0300

    i love singular classes

--- src/models/customer.js
+++ src/models/customer.js
@@ -1,8 +1,10 @@
-import { User } from "./user.js"
-
-export default class Customer extends User{
-    constructor(userId, userFirstName, userLastName, userCity,userAge, customerCreditCardNumber) {
-        super(userId, userFirstName, userLastName, userCity,userAge)
+export default class Customer {
+    constructor(id, firstName, lastName, city,age, customerCreditCardNumber) {
+        this.userId = id
+        this.userFirstName = firstName;
+        this.userLastName = lastName
+        this.userCity = city;
+        this.userAge = age
         this.creditCardNumber = customerCreditCardNumber
     }
 }
--- src/models/employee.js
+++ src/models/employee.js
@@ -1,12 +1,10 @@
-import User from "./user.js";
-
-export default class Employee extends User{
-    constructor(userId, userFirstName, userLastName, userCity,userAge, employeeSalary) {
-        this.userId = userId
-        this.userFirstName = userFirstName;
-        this.userLastName = userLastName
-        this.userCity = userCity;
-        this.userAge = userAge
+export default class Employee{
+    constructor(id, firstName, lastName, city,age, employeeSalary) {
+        this.userId = id
+        this.userFirstName = firstName;
+        this.userLastName = lastName
+        this.userCity = city;
+        this.userAge = age
         this.salary = employeeSalary
     }
 }
